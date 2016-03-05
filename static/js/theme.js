a1d1a29e99d71317117a67d13f028c0bcb32d903
Oleg <olegkriklivets@gmail.com>
2016-03-05 20:49:14 +0200

    add first page ans styles for page

---
+++ static/js/theme.js
@@ -0,0 +1,25 @@
+//this code needed for identification for vk 
+// vk init
+// VK.init({
+// 	apiId: 5238735, 
+// 	onlyWidgets: true
+// });
+
+// vk auth login
+// VK.Auth.getLoginStatus(function(response) {
+// 	if (response.session) {
+// 		if (window.location.pathname == "/") {
+// 			window.location.href = '/blogs/';
+// 		} 
+// 	} 
+// });
+
+
+// $(document).ready(function(){
+//     $("#comments").on("click", ".reply", function(event){
+//         event.preventDefault();
+//         var form = $("#comment_post").clone(true);
+//         form.find('.parent').val($(this).parent().parent().attr('id'));
+//         $(this).parent().append(form);
+//     });
+// });
