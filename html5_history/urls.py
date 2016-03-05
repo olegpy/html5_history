da3e97be548f0aa73db9e2f18800614262fc2f80
Oleg <olegkriklivets@gmail.com>
2016-03-05 20:48:55 +0200

    add first page ans styles for page

--- html5_history/urls.py
+++ html5_history/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from .views import index
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'html5_history.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
